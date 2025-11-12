36a9bdbcc22a475f228127d7666fb1443ec2d39c
Saro G. <saro@saromedia.com>
2025-11-12 11:27:46 -0500

    textproc/kibana92: new port: 9.2.1

---
+++ textproc/kibana92/files/patch-src_setup__node__env_node__version__validator.js
@@ -0,0 +1,21 @@
+--- src/setup_node_env/node_version_validator.js.orig	2025-11-12 16:09:54 UTC
++++ src/setup_node_env/node_version_validator.js
+@@ -16,14 +16,14 @@ if (!process.env.UNSAFE_DISABLE_NODE_VERSION_VALIDATIO
+   var currentVersion = process && process.version || null;
+   var rawRequiredVersion = pkg && pkg.engines && pkg.engines.node || null;
+   var requiredVersion = rawRequiredVersion ? 'v' + rawRequiredVersion : rawRequiredVersion;
+-  var isVersionValid = !!currentVersion && !!requiredVersion && currentVersion === requiredVersion;
++  var isVersionValid = !!currentVersion && !!requiredVersion && currentVersion >= requiredVersion;
+ 
+   // Validates current the NodeJS version compatibility when Kibana starts.
+   if (!isVersionValid) {
+-    var errorMessage = 'Kibana does not support the current Node.js version ' + currentVersion + '. Please use Node.js ' + requiredVersion + '.';
++    var errorMessage = 'Kibana does not support the current Node.js version ' + currentVersion + '. Please use Node.js >= ' + requiredVersion + '.';
+ 
+     // Actions to apply when validation fails: error report + exit.
+     console.error(errorMessage);
+     process.exit(1);
+   }
+-}
+\ No newline at end of file
++}
